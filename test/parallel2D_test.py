ba27581e55926774660da1d293d2f7ab73213a09
wband <william.beck.andrews@imtek.uni-freiburg.de>
2021-01-12 19:28:04 +0100

    added adaptive strain stepper

--- test/parallel2D_test.py
+++ test/parallel2D_test.py
@@ -11,9 +11,9 @@ import time
 
 class statdump():
 
-    def __init__(self, fname, avg_strain):
+    def __init__(self, fname):
         self.subiterations = 0
-        self.avg_strain = avg_strain
+        self.avg_strain = 0
         self.total_energy = 0
         self.elastic_energy = 0
         self.delta_phi = 0
@@ -38,7 +38,7 @@ def init_crack(obj):
         val = 0.0
         distcoord = np.abs(coords - np.array(obj.lens)/2)
         distcoord[0] = max(distcoord[0]-1.0,0)
-        dist = np.sqrt(np.sum(distcoord**2))  - obj.dx[0]/2.0
+        dist = np.sqrt(np.sum(distcoord**2))  - obj.dx[0]/4.0
         if (dist < 2.0**0.5):
             val = (1.0-dist/2.0**0.5)**2
         if (dist < 0):
@@ -48,6 +48,7 @@ def init_crack(obj):
 
 def iteration(obj,statobj):
     delta_energy = 1.0
+    delta_energy_old = 1e6
     energy_old = obj.total_energy + 0.0
     statobj.subiterations = 0
     statobj.strain_time = 0.0
@@ -62,61 +63,62 @@ def iteration(obj,statobj):
         phit = time.time()
         delta_energy = abs(obj.total_energy-energy_old)
         energy_old = obj.total_energy + 0.0
-        
+
         statobj.subiterations += 1
         statobj.strain_time += straint-start
         statobj.phi_time += phit-straint
         if(obj.comm.rank == 0):
             print('delta energy = ',delta_energy)
+        if((delta_energy > delta_energy_old + obj.delta_energy_tol)
+               and (obj.strain_step > 0.001) and (statobj.subiterations > 1)):
+            obj.F_tot[1,1] -= obj.strain_step
+            obj.strain_step /= 2
+            obj.F_tot[1,1] += obj.strain_step
+            statobj.subiterations = 1
+            delta_energy_old = 1e6
+            if (obj.comm.rank == 0):
+                print('non-monotonicity of energy convergence detected, strain step reduced to ', obj.strain_step)
+        else:
+            delta_energy_old = delta_energy
 
 def run_test(obj):
-    nmax = 24
-    avg_strain_all = np.linspace(0.05,0.073,num=nmax) #np.linspace(0.07,0.093,num=nmax)
-    obj.strain_step = avg_strain_all[1] - avg_strain_all[0]
-    if(obj.comm.rank == 0):
-        print(avg_strain_all)
-    
-    delta_phi = np.zeros(nmax)
-    total_energy = np.zeros(nmax)
-    strain_time = np.zeros(nmax)
-    phi_time = np.zeros(nmax)
+    nmax = 50
+    obj.strain_step = 0.008
+    strain_time = []
+    phi_time = []
+    subiterations = []
+    obj.F_tot[1,1] = 0.0
     obj.phi_old = obj.phi.array() + 0.0
     fieldoutputname = 'test.nc'
     obj.muOutput(fieldoutputname,new=True)
-    stats = statdump('stats.json',avg_strain_all[0])
+    stats = statdump('stats.json',)
     if(obj.comm.rank == 0): 
         stats.clear()
-    for n in range(0,nmax):
-        obj.F_tot[1,1] = avg_strain_all[n]
+    n = 0
+    while (n < nmax):
         iteration(obj,stats)
-        
-        stats.avg_strain = avg_strain_all[n]
+        stats.avg_strain = obj.F_tot[1,1]
         stats.total_energy = obj.total_energy
         stats.delta_phi = obj.integrate(obj.phi.array()-obj.phi_old)
         stats.strain_energy = obj.integrate((1.0-obj.phi.array())**2*obj.straineng.array())
-        delta_phi[n] = stats.delta_phi
-        total_energy[n] = stats.total_energy 
-        strain_time[n] = stats.strain_time
-        phi_time[n] = stats.phi_time
-
+        strain_time.append(stats.strain_time)
+        phi_time.append(stats.phi_time)
+        subiterations.append(stats.subiterations)
         obj.phi_old = np.maximum(obj.phi.array(),obj.phi_old)
         if(obj.comm.rank == 0):
-            print('strain: ', avg_strain_all[n], 'energy: ',total_energy[n],'delta phi',delta_phi[n])
+            print('strain: ', stats.avg_strain, 'energy: ',stats.total_energy,'delta phi',stats.delta_phi)
             stats.dump()
         obj.muOutput(fieldoutputname)
         #obj.crappyIO('fields'+str(n).rjust(2,'0'))
-        if(stats.strain_energy < 1.0):
+        if((stats.strain_energy < 1.0) and (n > 1)):
             break
+        obj.F_tot[1,1] += obj.strain_step
+        n += 1
     
     if(obj.comm.rank == 0):
         print('strain time:', np.sum(strain_time))
         print('phi time:', np.sum(phi_time))
-        np.save('total_energy',total_energy)
-        np.save('delta_phi',delta_phi)
-        np.save('avg_strain',avg_strain_all)
-        np.save('strain_time',strain_time)
-        np.save('phi_time',phi_time)
-
+        print('total subiterations:', np.sum(subiterations))
 
 nx=127
 Lx=20
@@ -127,7 +129,7 @@ f.solver_tol = 1e-10
 f.title = 'test'
 f.phi.array()[...] = init_crack(f)
 
-structobj = makestruct.randomfield(Lx=Lx,nx=nx,lamb=1,sigma=0.3,mu=1,minimum_val=0)
+structobj = makestruct.randomfield(Lx=Lx,nx=nx,lamb=2,sigma=0.3,mu=1,minimum_val=0)
 if(f.comm.rank == 0):
     structobj.makestruct2D()
 f.comm.barrier()

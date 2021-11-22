bc59dab5ff49b0bd7e37ac4b227af24e89a07bc4
wband <william.beck.andrews@imtek.uni-freiburg.de>
2021-11-22 16:46:25 +0100

    added volumetric/deviatoric split mechanics

--- src/mechanics.py
+++ src/mechanics.py
@@ -111,9 +111,9 @@ class anisotropic_tc():
         return (obj.interp.energy(obj.phi.array())*obj.straineng.array() + self.get_compressive_energy(obj))
 
 # copy of isotropic_tc that shuts off driving force for compression
-class nonvar_tc():
+class nonvar_phi_tc():
     def __init__(self):
-        self.name = "nonvar_tc"
+        self.name = "nonvar_phi_tc"
             
     def initialize_material(self,obj):
         material = msp.material.MaterialLinearElastic4_2d.make(obj.cell, "material_small")
@@ -141,18 +141,204 @@ class nonvar_tc():
             obj.straineng.array()[tuple(pixel)] = Cxval*(np.maximum(np.sum(pstrains),0)**2*
                 lamb_factor*0.5 + np.sum(np.maximum(pstrains,0)**2)*mu_factor)
 
-    def get_compressive_energy(self,obj):
+    def get_elastic_energy(self, obj):
         lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
         mu_factor = 1/2/(1+obj.Poisson)
-        compressive_energy = np.zeros_like(obj.straineng.array())
+        elastic_energy = np.zeros_like(obj.straineng.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(obj.strain.array()[:,0,pixel[0],pixel[1]],(obj.dim,obj.dim))            
+            obj.strain.array()[:,0,pixel[0],pixel[1]] = strain.flatten()
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            elastic_energy[tuple(pixel)] = 0.5*obj.Cx.array()[tuple(pixel)]*(2.0*mu_factor*(strain**2).sum()
+                + lamb_factor*trace**2)
+
+# copy of anisotropic_tc that uses isotropic elastic energy to evolve phi
+class nonvar_contact_tc():
+    def __init__(self):
+        self.name = "nonvar_phi_tc"
+            
+    def initialize_material(self,obj):
+        material = msp.material.MaterialPhaseFieldFracture_2d.make(obj.cell, "material_small",obj.ksmall)
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+             pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+             material.add_pixel(pixel_id, obj.Cx.array()[tuple(pixel)], 
+                 obj.Poisson, obj.phi.array()[tuple(pixel)])
+        return material
+
+    def update_material(self,obj):            
+        ### set current material properties
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='C')
+            obj.material.set_phase_field(pixel_id, obj.phi.array()[tuple(pixel)])
+
+    def get_elastic_coupling(self, obj, strain_result):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(strain_result.grad[pixel_id*obj.dim**2:(pixel_id+1)*obj.dim**2],(obj.dim,obj.dim))
+            obj.strain.array()[:,0,pixel[0],pixel[1]] = strain.flatten()
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            obj.straineng.array()[tuple(pixel)] = 0.5*obj.Cx.array()[tuple(pixel)]*(2.0*mu_factor*(strain**2).sum()
+                + lamb_factor*trace**2)
+
+    def get_elastic_energy(self,obj):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        elastic_energy = np.zeros_like(obj.straineng.array())
         for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
             pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
             strain = np.reshape(obj.strain.array()[:,0,pixel[0],pixel[1]],(obj.dim,obj.dim))
             pstrains = np.linalg.eigvalsh(strain)
-            compressive_energy[tuple(pixel)] = Cxval*(np.minimum(np.sum(pstrains),0)**2*lamb_factor*0.5 +
+            elastic_energy[tuple(pixel)] += Cxval*(np.minimum(np.sum(pstrains),0)**2*lamb_factor*0.5 +
                 np.sum(np.minimum(pstrains,0)**2)*mu_factor)
-        return compressive_energy
+            elastic_energy[tuple(pixel)] += Cxval*(np.maximum(np.sum(pstrains),0)**2*lamb_factor*0.5 +
+                np.sum(np.maximum(pstrains,0)**2)*mu_factor)*obj.interp.energy(obj.phi.array()[pixel[0],pixel[1]])
+        return elastic_energy
 
+
+class voldev_tc():
+    def __init__(self):
+        self.name = "voldev_tc"
+
+    def initialize_material(self,obj):
+        material = msp.material.MaterialPhaseFieldFracture2_2d.make(obj.cell, "material_small",obj.ksmall)
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+             pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+             material.add_pixel(pixel_id, obj.Cx.array()[tuple(pixel)], 
+                 obj.Poisson, obj.phi.array()[tuple(pixel)])
+        return material
+
+    def update_material(self,obj):            
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='C')
+            obj.material.set_phase_field(pixel_id, obj.phi.array()[tuple(pixel)])
+    
+    def get_elastic_coupling(self,obj,strain_result):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        K_factor = lamb_factor + 2/3*mu_factor
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(strain_result.grad[pixel_id*obj.dim**2:(pixel_id+1)*obj.dim**2],(obj.dim,obj.dim))
+            obj.strain.array()[:,0,pixel[0],pixel[1]] = strain.flatten()
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            obj.straineng.array()[tuple(pixel)] = Cxval*(np.maximum(trace,0)**2*
+                K_factor*0.5 + np.sum((strain-np.eye(obj.dim)*trace/3)**2)*mu_factor)
+        
+    def get_compressive_energy(self,obj):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        K_factor = lamb_factor + 2/3*mu_factor
+        compressive_energy = np.zeros_like(obj.straineng.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(obj.strain.array()[:,0,pixel[0],pixel[1]],(obj.dim,obj.dim))
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            compressive_energy[tuple(pixel)] = Cxval*(np.minimum(trace,0)**2*K_factor*0.5)
+        return compressive_energy
+        
     def get_elastic_energy(self, obj):
         return (obj.interp.energy(obj.phi.array())*obj.straineng.array() + self.get_compressive_energy(obj))
+
+# non-variational model with volumetric/deviatoric split for contact and spectral split for phase field
+class nonvar_both_tc():
+    def __init__(self):
+        self.name = "nonvar_both_tc"
+
+    def initialize_material(self,obj):
+        material = msp.material.MaterialPhaseFieldFracture2_2d.make(obj.cell, "material_small",obj.ksmall)
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+             pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+             material.add_pixel(pixel_id, obj.Cx.array()[tuple(pixel)],
+                 obj.Poisson, obj.phi.array()[tuple(pixel)])
+        return material
+
+    def update_material(self,obj):
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='C')
+            obj.material.set_phase_field(pixel_id, obj.phi.array()[tuple(pixel)])
+
+    def get_elastic_coupling(self,obj,strain_result):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(strain_result.grad[pixel_id*obj.dim**2:(pixel_id+1)*obj.dim**2],(obj.dim,obj.dim))
+            obj.strain.array()[:,0,pixel[0],pixel[1]] = strain.flatten()
+            pstrains = np.linalg.eigvalsh(strain)
+            obj.straineng.array()[tuple(pixel)] = Cxval*(np.maximum(np.sum(pstrains),0)**2*
+                lamb_factor*0.5 + np.sum(np.maximum(pstrains,0)**2)*mu_factor)
+
+    def get_elastic_energy(self,obj):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        K_factor = lamb_factor + 2/3*mu_factor
+        elastic_energy = np.zeros_like(obj.straineng.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(obj.strain.array()[:,0,pixel[0],pixel[1]],(obj.dim,obj.dim))
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            elastic_energy[tuple(pixel)] += Cxval*(np.maximum(trace,0)**2*
+                K_factor*0.5 + np.sum((strain-np.eye(obj.dim)*trace/3)**2)*mu_factor)
+            elastic_energy[tuple(pixel)] += Cxval*(np.minimum(trace,0)**2*K_factor*0.5)
+        return elastic_energy
+
+class nonvar_phi_vd():
+    def __init__(self):
+        self.name = "nonvar_phi_vd"
+
+    def initialize_material(self,obj):
+        material = msp.material.MaterialLinearElastic4_2d.make(obj.cell, "material_small")
+        interp = obj.interp.energy(obj.phi.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            material.add_pixel(pixel_id, obj.Cx.array()[tuple(pixel)]*interp[tuple(pixel)], obj.Poisson)
+        return material
+
+    def update_material(self, obj):
+        ### set current material properties
+        interp = obj.interp.energy(obj.phi.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='C')
+            obj.material.set_youngs_modulus(pixel_id,Cxval*interp[tuple(pixel)])
+
+    def get_elastic_coupling(self,obj,strain_result):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        K_factor = lamb_factor + 2/3*mu_factor
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(strain_result.grad[pixel_id*obj.dim**2:(pixel_id+1)*obj.dim**2],(obj.dim,obj.dim))
+            obj.strain.array()[:,0,pixel[0],pixel[1]] = strain.flatten()
+            trace = 0.0
+            for k in range(0,obj.dim):
+                trace += strain[k,k]
+            obj.straineng.array()[tuple(pixel)] = Cxval*(np.maximum(trace,0)**2*
+                K_factor*0.5 + np.sum((strain-np.eye(obj.dim)*trace/3)**2)*mu_factor)
+        
+    def get_elastic_energy(self,obj):
+        lamb_factor = obj.Poisson/(1+obj.Poisson)/(1-2*obj.Poisson)
+        mu_factor = 1/2/(1+obj.Poisson)
+        elastic_energy = np.zeros_like(obj.straineng.array())
+        for pixel, Cxval in np.ndenumerate(obj.Cx.array()):
+            pixel_id = np.ravel_multi_index(pixel, obj.fftengine.nb_subdomain_grid_pts, order='F')
+            strain = np.reshape(obj.strain.array()[:,0,pixel[0],pixel[1]],(obj.dim,obj.dim))
+            pstrains = np.linalg.eigvalsh(strain)
+            elastic_energy[tuple(pixel)] += Cxval*(np.minimum(np.sum(pstrains),0)**2*lamb_factor*0.5 +
+                np.sum(np.minimum(pstrains,0)**2)*mu_factor)
+            elastic_energy[tuple(pixel)] += Cxval*(np.maximum(np.sum(pstrains),0)**2*lamb_factor*0.5 +
+                np.sum(np.maximum(pstrains,0)**2)*mu_factor)*obj.interp.energy(obj.phi.array()[pixel[0],pixel[1]])
+        return elastic_energy
 
